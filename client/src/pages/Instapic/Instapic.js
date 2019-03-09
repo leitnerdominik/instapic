3e29954cc38162c1701dc3f5cee399815529bcb8
Dominik Leitner <bigtoasti@gmail.com>
2019-03-09 22:22:02 +0100

    add loading spinner and a when no post was found

--- client/src/pages/Instapic/Instapic.js
+++ client/src/pages/Instapic/Instapic.js
@@ -5,43 +5,13 @@ import Button from '../../components/Button/Button';
 import Layout from '../../container/Layout/Layout';
 import ImageList from '../../components/ImageList/ImageList';
 import PostImage from '../../container/PostImage/PostImage';
+import Spinner from '../../components/Spinner/Spinner';
 
 import * as action from '../../store/actions/index';
-
-import image1 from '../../images/image1.jpg';
-import image2 from '../../images/image2.jpg';
-import image3 from '../../images/image3.jpg';
 
 import classes from './Instapic.module.css';
 
 class Instapic extends Component {
-  // state = {
-  //   images: [
-  //     {
-  //       id: 'image1',
-  //       title: 'image1',
-  //       description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
-  //       path: image1,
-  //       alt: 'image1',
-  //     },
-  //     {
-  //       id: 'image2',
-  //       title: 'image2',
-  //       description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
-  //       path: image2,
-  //       alt: 'image2',
-  //     },
-  //     {
-  //       id: 'image3',
-  //       title: 'image3',
-  //       description: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam`,
-  //       path: image3,
-  //       alt: 'image3',
-  //     },
-  //   ],
-  //   showPostImage: false,
-  // };
-
   state = {
     posts: [],
     showPostImage: false,
@@ -50,13 +20,13 @@ class Instapic extends Component {
   componentDidMount() {
     this.props.onFetchPosts();
   }
-  
+
   togglePostImage = () => {
     this.setState(prevState => ({
       showPostImage: !prevState.showPostImage,
     }));
   };
-  
+
   render() {
     return (
       <Fragment>
@@ -67,7 +37,15 @@ class Instapic extends Component {
           <div className={classes.Options}>
             <Button onClick={this.togglePostImage}>Upload Image</Button>
           </div>
-          {this.props.posts.length > 0 ? (
+          {this.props.loadingPosts && (
+            <div className={classes.Center}>
+              <Spinner />
+            </div>
+          )}
+          {this.props.posts.length <= 0 && !this.props.loadingPosts ?  (
+            <p className={classes.Center}>No posts found!</p>
+          ) : null}
+          {this.props.posts.length > 0 && !this.props.loadingPosts ? (
             <ImageList images={this.props.posts} />
           ) : null}
         </Layout>
@@ -78,9 +56,10 @@ class Instapic extends Component {
 
 const mapStateToProps = state => {
   return {
-    posts: state.post.posts,
-  }
-}
+    posts: state.posts.posts,
+    loadingPosts: state.posts.loading,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
